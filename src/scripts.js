da73cb2b983931ab86140451bd2275d4cc3403b4
Jelena Jovicic <jejovicic@gmail.com>
2024-08-02 16:41:50 +0200

    Refactor

--- src/scripts.js
+++ src/scripts.js
@@ -37,13 +37,13 @@ var statsAverageGuesses = document.querySelector('#stats-average-guesses');
 // Event Listeners
 window.addEventListener('load', setGame);
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener('keyup', function () { moveToNextInput(event) });
-}
+inputs.forEach(input => input.addEventListener('keyup', (event) => {
+  moveToNextInput(event)
+}));
 
-for (var i = 0; i < keyLetters.length; i++) {
-  keyLetters[i].addEventListener('click', function () { clickLetter(event) });
-}
+keyLetters.forEach(keyLetter => keyLetter.addEventListener("click", (event) => {
+  clickLetter(event)
+}));
 
 guessButton.addEventListener('click', submitGuess);
 
@@ -67,13 +67,13 @@ function getRandomWord() {
 }
 
 function updateInputPermissions() {
-  for (var i = 0; i < inputs.length; i++) {
-    if (!inputs[i].id.includes(`-${currentRow}-`)) {
-      inputs[i].disabled = true;
+  inputs.forEach(input => {
+    if (!input.id.includes(`-${currentRow}-`)) {
+      input.disabled = true;
     } else {
-      inputs[i].disabled = false;
+      input.disabled = false;
     }
-  }
+  })
   inputs[0].focus();
 }
 
@@ -93,29 +93,24 @@ function clickLetter(e) {
   var activeInput = null;
   var activeIndex = null;
 
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].id.includes(`-${currentRow}-`) && !inputs[i].value && !activeInput) {
-      activeInput = inputs[i];
-      activeIndex = i;
+  inputs.forEach((input, index) => {
+    if (input.id.includes(`-${currentRow}-`) && !input.value && !activeInput) {
+      activeInput = input;
+      activeIndex = index;
     }
-  }
+  })
   // after clicking the letter, focus on next input
   activeInput.value = e.target.innerText;
   if (inputs.length > activeIndex + 1) { //added - stop changing focus if there's no next input
     inputs[activeIndex + 1].focus();
   }
-
 }
 
 function submitGuess() {
   if (checkIsWord()) {
     errorMessage.innerText = '';
     compareGuess();
-    if (checkForWin()) {
-      recordGameStats(checkForWin())
-      setTimeout(endGame, 1000);
-
-    } else if (currentRow < 6) {
+    if (currentRow < 6 && !checkForWin()) {
       changeRow();
     } else {
       recordGameStats(checkForWin())
@@ -126,61 +121,58 @@ function submitGuess() {
   }
 }
 
-
-
 function checkIsWord() {
   guess = '';
-
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].id.includes(`-${currentRow}-`)) {
-      guess += inputs[i].value;
+  inputs.forEach(input => {
+    if(input.id.includes(`-${currentRow}-`)) {
+      guess += input.value
     }
-  }
-
+  })
   return words.includes(guess);
 }
 
 function compareGuess() {
   var guessLetters = guess.split('');
 
-  for (var i = 0; i < guessLetters.length; i++) {
-
-    if (winningWord.includes(guessLetters[i]) && winningWord.split('')[i] !== guessLetters[i]) {
-      updateBoxColor(i, 'wrong-location');
-      updateKeyColor(guessLetters[i], 'wrong-location-key');
-    } else if (winningWord.split('')[i] === guessLetters[i]) {
-      updateBoxColor(i, 'correct-location');
-      updateKeyColor(guessLetters[i], 'correct-location-key');
+  guessLetters.forEach((letter, index) => {
+    if (winningWord.includes(letter) && winningWord.split('')[index] !== letter) {
+      updateBoxColor(index, 'wrong-location');
+      updateKeyColor(letter, 'wrong-location-key');
+    } else if (winningWord.split('')[index] === letter) {
+      updateBoxColor(index, 'correct-location');
+      updateKeyColor(letter, 'correct-location-key');
     } else {
-      updateBoxColor(i, 'wrong');
-      updateKeyColor(guessLetters[i], 'wrong-key');
+      updateBoxColor(index, 'wrong');
+      updateKeyColor(letter, 'wrong-key');
     }
-  }
+  })
+
+
 
 }
 
 function updateBoxColor(letterLocation, className) {
   var row = [];
 
-  for (var i = 0; i < inputs.length; i++) {
-    if (inputs[i].id.includes(`-${currentRow}-`)) {
-      row.push(inputs[i]);
+  inputs.forEach(input => {
+    if (input.id.includes(`-${currentRow}-`)) {
+      row.push(input);
     }
-  }
+  })
 
   row[letterLocation].classList.add(className);
 }
 
 function updateKeyColor(letter, className) {
-  var keyLetter = null;
+  var keySpan = null;
 
-  for (var i = 0; i < keyLetters.length; i++) {
-    if (keyLetters[i].innerText === letter) {
-      keyLetter = keyLetters[i];
+  keyLetters.forEach(keyLetter => {
+    if (keyLetter.innerText === letter) {
+      keySpan = keyLetter;
     }
-  }
-
-  keyLetter.classList.add(className);
+  })
+  
+  keySpan.classList.add(className);
 }
 
 function checkForWin() {
@@ -208,7 +200,7 @@ function changeGameOverText() {
     let gameOverGuessGrammar = "";
     if (currentRow >= 2) {
       gameOverGuessGrammar = "es"
-    } 
+    }
     gameOverHeading.textContent = "Yay!"
     gameOverInfo.textContent = `You did it! It took you ${currentRow} guess${gameOverGuessGrammar} to find the correct word.`
   }
@@ -221,13 +213,13 @@ function changeGameOverText() {
 function changeStatText() {
   const totalGames = gamesPlayed.length;
   const wonGames = gamesPlayed.filter(game => game.solved);
-  const percentCorrect = wonGames.length / totalGames * 100
+  const percentCorrect = Math.round((wonGames.length / totalGames * 100) * 100) / 100
   const guesses = wonGames.map(game => game.guesses)
   const sumOfGuesses = guesses.reduce((acc, curr) => {
     acc += curr;
     return acc;
   }, 0)
-  const averageGuesses = sumOfGuesses / guesses.length
+  const averageGuesses = Math.round((sumOfGuesses / guesses.length) * 100) / 100
   statsTotalGames.textContent = totalGames;
   statsPercentCorrect.textContent = percentCorrect;
   statsAverageGuesses.textContent = averageGuesses;
@@ -242,16 +234,16 @@ function startNewGame() {
 }
 
 function clearGameBoard() {
-  for (var i = 0; i < inputs.length; i++) {
-    inputs[i].value = '';
-    inputs[i].classList.remove('correct-location', 'wrong-location', 'wrong');
-  }
+  inputs.forEach(input => {
+    input.value = '';
+    input.classList.remove('correct-location', 'wrong-location', 'wrong')
+  })
 }
 
 function clearKey() {
-  for (var i = 0; i < keyLetters.length; i++) {
-    keyLetters[i].classList.remove('correct-location-key', 'wrong-location-key', 'wrong-key');
-  }
+  keyLetters.forEach(keyLetter => {
+    keyLetter.classList.remove('correct-location-key', 'wrong-location-key', 'wrong-key');
+  })
 }
 
 // Change Page View Functions
